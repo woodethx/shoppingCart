a066fa6fe1b6e64f069aa4c8c874341c4af0aca1
Ethan Wood <woodethx@gmail.com>
2026-01-16 11:26:27 -0600

    Add - Route file and nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,31 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Link, Outlet} from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <p>Hello World</p>
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <Link to={'/'}>Home</Link>
+            </li>
+            <li>
+              <Link to={'/shop'}>Shop</Link>
+            </li>
+            <li>
+              <Link to={'/cart'}>Cart</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+        <Outlet></Outlet>
+      </main>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import routes from "./Routes.jsx";
 import './index.css'
 import App from './App.jsx'
 
+const router = createBrowserRouter(routes);
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
